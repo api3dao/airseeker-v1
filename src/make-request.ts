00c92536e27f67aaa5bbae995024bc0e53593d59
Emanuel Tesar <e.tesarr@gmail.com>
2022-04-07 18:14:29 +0200

    Pass required parameters to makeRequest function

--- src/make-request.ts
+++ src/make-request.ts
@@ -1,6 +1,7 @@
 import { GoResult } from '@api3/promise-utils';
+import { Gateway, Template } from './validation';
 
 // TODO: https://api3dao.atlassian.net/browse/BEC-295
-export const makeSignedDataGatewayRequest = async () => {
+export const makeSignedDataGatewayRequest = async (_gateway: Gateway, _template: Template) => {
   return 123 as any as GoResult<any>;
 };
