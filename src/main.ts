99c9a4477767c9d9b5647c7181b3a4a623826366
Emanuel Tesar <e.tesarr@gmail.com>
2022-04-07 14:39:51 +0200

    Implement beacon data fetching

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import * as path from 'path';
+import { performance } from 'perf_hooks';
+import { uniq } from 'lodash';
+import { GoResult } from '@api3/promise-utils';
 import { loadConfig } from './config';
+import { saveBeaconValue } from './beacon-value-storage';
 
-const DATA_FETCH_FREQUENCY_MS = 5000;
 const BEACON_UPDATE_FREQUENCY_MS = 10_000;
-
 let stopSignalReceived = false;
+
+// ============================
+// Load configuration
+// ============================
+const config = loadConfig(path.join(__dirname, '..', 'config', 'config.json'), process.env);
 
 const handleStopSignal = (signal: string) => {
   console.log(`Signal ${signal} received`);
@@ -13,12 +20,46 @@ const handleStopSignal = (signal: string) => {
   stopSignalReceived = true;
 };
 
-const fetchData = async () => {
-  console.log('Fetching data');
+const initiateFetchingBeaconData = async () => {
+  console.log('Initiating fetching all beacon data');
+
+  const beaconIdsToUpdate = uniq(
+    Object.values(config.triggers.beaconUpdates).flatMap((beaconUpdatesPerSponsor) => {
+      return Object.values(beaconUpdatesPerSponsor).flatMap((beaconUpdate) =>
+        beaconUpdate.beacons.flatMap((b) => b.beaconId)
+      );
+    })
+  );
+
+  beaconIdsToUpdate.forEach((id) => {
+    fetchBeaconData(id);
+  });
+};
+
+// TODO: https://api3dao.atlassian.net/browse/BEC-295
+const makeSignedDataGatewayRequest = () => {
+  return 123 as any as GoResult<any>;
+};
+
+const fetchBeaconData = async (beaconId: string) => {
+  console.log(`Fetching beacon data for: ${beaconId}`);
+
+  const startTimestamp = performance.now();
+  const { fetchInterval } = config.beacons[beaconId];
+
+  const goRes = await makeSignedDataGatewayRequest();
+  if (!goRes.success) {
+    console.log(`Unable to call signed data gateway. Reason: ${goRes.error}`);
+  } else {
+    saveBeaconValue(beaconId, goRes.data);
+  }
+
   if (!stopSignalReceived) {
+    const duration = performance.now() - startTimestamp;
+    const waitTime = Math.max(0, fetchInterval - duration);
     setTimeout(() => {
-      fetchData();
-    }, DATA_FETCH_FREQUENCY_MS);
+      fetchBeaconData(beaconId);
+    }, waitTime);
   }
 };
 
@@ -31,14 +72,7 @@ const updateBeacons = async () => {
   }
 };
 
-// ============================
-// Load configuration
-// ============================
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const config = loadConfig(path.join(__dirname, '..', 'config', 'config.json'), process.env);
-// do something with config
-
-fetchData();
+initiateFetchingBeaconData();
 updateBeacons();
 
 process.on('SIGINT', handleStopSignal);
