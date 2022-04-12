0555da25d2537b4e3a6a16509cc578ea747e076e
Michal Kimle <kimle.michal@gmail.com>
2022-04-12 14:05:33 +0200

    Add provider initialization

--- src/state.ts
+++ src/state.ts
@@ -1,15 +1,26 @@
+import { ethers } from 'ethers';
+import { DapiServer } from '@api3/airnode-protocol-v1';
 import { BeaconId, SignedData } from './validation';
 
 export type BeaconValueStorage = Record<BeaconId, SignedData>;
+export type Provider = {
+  contract: DapiServer;
+  rpcProvider: ethers.providers.StaticJsonRpcProvider;
+  chainId: string;
+};
+// chainId => Provider[]
+export type Providers = Record<string, Provider[]>;
 
 export interface State {
   stopSignalReceived: boolean;
   beaconValues: BeaconValueStorage;
+  providers: Providers;
 }
 
 export const createDefaultState: () => State = () => ({
   stopSignalReceived: false,
   beaconValues: {},
+  providers: {},
 });
 
 // TODO: Freeze the state in development mode
