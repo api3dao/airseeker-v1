b1748e7c697fd3e22584d9b1145b058fbaf449ed
Emanuel Tesar <e.tesarr@gmail.com>
2022-04-13 09:48:25 +0200

    Rename testing-services script

--- testing-services.config.js
+++ testing-services.config.js
@@ -2,18 +2,6 @@
 module.exports = {
   apps: [
     {
-      name: 'ethereum-node',
-      script: 'hardhat node',
-      env: {
-        NODE_ENV: 'development',
-      },
-      // Logs
-      log_date_format: 'YYYY-MM-DD HH:mm:ss',
-      error_file: 'logs/ethereum-node.log',
-      out_file: 'logs/ethereum-node.log',
-      merge_logs: true,
-    },
-    {
       name: 'mock-server',
       script: 'ts-node ./test/server.ts',
       env: {
