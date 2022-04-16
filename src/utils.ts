ad904b66f255bb548a41f3b07a2224d883c6eda8
Michal Kimle <kimle.michal@gmail.com>
2022-04-16 19:05:48 +0200

    Add transaction count fetching

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,3 @@
 export const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+export const shortenAddress = (address: string) => address.replace(address.substring(5, 38), '...');
