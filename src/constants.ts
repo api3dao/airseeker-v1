c630995c6db6a48e5abffcc011c8688cac671a60
Michal Kimle <kimle.michal@gmail.com>
2022-04-20 12:03:46 +0200

    Move `INT224_MIN` and `INT224_MAX` to `constants.ts`

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
+import { ethers } from 'ethers';
+
 export const INFINITE_RETRIES = 100_000;
 export const GATEWAY_TIMEOUT_MS = 5_000;
 export const PROVIDER_TIMEOUT_MS = 5_000;
@@ -9,3 +11,7 @@ export const BASE_FEE_MULTIPLIER = 2;
 export const PROTOCOL_ID = '5';
 // The default gas limit for transactions
 export const GAS_LIMIT = 500_000;
+// Solidity type(int224).min
+export const INT224_MIN = ethers.BigNumber.from(2).pow(ethers.BigNumber.from(223)).mul(ethers.BigNumber.from(-1));
+// Solidity type(int224).max
+export const INT224_MAX = ethers.BigNumber.from(2).pow(ethers.BigNumber.from(223)).sub(ethers.BigNumber.from(1));
