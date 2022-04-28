27d22e49d70419382698860e5b31bac78b77bab0
Michal Kimle <kimle.michal@gmail.com>
2022-04-28 11:26:23 +0200

    Use only unquoted secrets for Airseeker

--- scripts/run-locally.sh
+++ scripts/run-locally.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
+
+SECRETS_FILE=$(dirname $(readlink -f "$0"))/../config/secrets.env
 
 # Enable automatic export of variables
 set -a
 # Source variables from secrets.env
-. $(dirname $(readlink -f "$0"))/../config/secrets.env
+source <(grep -v "^\s*#" ${SECRETS_FILE} | sed 's/\([^=]*\)=\(.*\)/\1=\"\2"/')
 # Disable automatic export of variables
 set +a
 
