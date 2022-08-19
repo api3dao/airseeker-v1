97370ccbc0c8f7b08b041fd507b64864ddd7dbf3
Michal Kimle <kimle.michal@gmail.com>
2022-08-19 08:29:39 +0200

    Remove Docker builds

--- docker/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Starting cron (as root) to run logrotate (as non-root) once a day
-crond
-
-# Starting pm2 under non-root user
-# Using `exec` to pass signals from shell to the binary
-exec su-exec ${name}:${name} pm2-runtime /app/airseeker-service.config.js --env production
