0e4c6ce5b25ae80e216e315b0ef5f816b12436e4
bdrhn9 <bedirhan@api3.org>
2022-10-03 14:30:43 +0300

    Update airseeker.feature e2e test

--- test/fixtures/config.ts
+++ test/fixtures/config.ts
@@ -9,11 +9,19 @@ export const buildAirseekerConfig = () => ({
       airnode: '0xA30CA71Ba54E83127214D3271aEA8F5D6bD4Dace',
       templateId: '0xea30f92923ece1a97af69d450a8418db31be5a26a886540a13c09c739ba8eaaa',
       fetchInterval: 15,
+      method: 'v0.6.5'
     },
     '0xbf7ce55d109fd196de2a8bf1515d166c56c9decbe9cb473656bbca30d5743990': {
       airnode: '0xA30CA71Ba54E83127214D3271aEA8F5D6bD4Dace',
       templateId: '0x0bbf5f2ec4b0e9faf5b89b4ddbed9bdad7a542cc258ffd7b106b523aeae039a6',
       fetchInterval: 15,
+      method: 'v0.9.0'
+    },
+    '0x9b5825decf1232f79d3408fb6f7eeb7050fd88037f6517a94914e7d01ccd0cef': {
+      airnode: '0xA30CA71Ba54E83127214D3271aEA8F5D6bD4Dace',
+      templateId: '0xc43a79e09e53edfdb601acef6b52000ecb7da353aee45255c518fb9d978d9283',
+      fetchInterval: 15,
+      method: 'direct'
     },
   },
   beaconSets: {
@@ -76,6 +84,17 @@ export const buildAirseekerConfig = () => ({
       parameters:
         '0x3173737373730000000000000000000000000000000000000000000000000000746f00000000000000000000000000000000000000000000000000000000000055534400000000000000000000000000000000000000000000000000000000005f74797065000000000000000000000000000000000000000000000000000000696e7432353600000000000000000000000000000000000000000000000000005f70617468000000000000000000000000000000000000000000000000000000726573756c7400000000000000000000000000000000000000000000000000005f74696d65730000000000000000000000000000000000000000000000000000313030303030300000000000000000000000000000000000000000000000000066726f6d000000000000000000000000000000000000000000000000000000004254430000000000000000000000000000000000000000000000000000000000',
     },
+    '0xc43a79e09e53edfdb601acef6b52000ecb7da353aee45255c518fb9d978d9283': {
+      endpointId: '0x13dea3311fe0d6b84f4daeab831befbc49e19e6494c41e9e065a09c3c68f43b6',
+      parameters:
+        '0x3173737373730000000000000000000000000000000000000000000000000000746f00000000000000000000000000000000000000000000000000000000000055534400000000000000000000000000000000000000000000000000000000005f74797065000000000000000000000000000000000000000000000000000000696e7432353600000000000000000000000000000000000000000000000000005f70617468000000000000000000000000000000000000000000000000000000726573756c7400000000000000000000000000000000000000000000000000005f74696d65730000000000000000000000000000000000000000000000000000313030303030300000000000000000000000000000000000000000000000000066726f6d000000000000000000000000000000000000000000000000000000004c54430000000000000000000000000000000000000000000000000000000000',
+    },
+  },
+  endpoints: {
+    '0x13dea3311fe0d6b84f4daeab831befbc49e19e6494c41e9e065a09c3c68f43b6': {
+      oisTitle: 'Currency Converter API',
+      endpointName: 'convertToUSD',
+    },
   },
   triggers: {
     dataFeedUpdates: {
@@ -92,6 +111,11 @@ export const buildAirseekerConfig = () => ({
               deviationThreshold: 0.2,
               heartbeatInterval: 86400,
             },
+            {
+              beaconId: '0x9b5825decf1232f79d3408fb6f7eeb7050fd88037f6517a94914e7d01ccd0cef',
+              deviationThreshold: 0.2,
+              heartbeatInterval: 86400,
+            },
           ],
           beaconSets: [
             {
@@ -105,6 +129,109 @@ export const buildAirseekerConfig = () => ({
       },
     },
   },
+  ois: [
+    {
+      oisFormat: '1.1.1',
+      version: '1.2.3',
+      title: 'Currency Converter API',
+      apiSpecifications: {
+        servers: [
+          {
+            url: 'http://localhost:5432',
+          },
+        ],
+        paths: {
+          '/convert': {
+            get: {
+              parameters: [
+                {
+                  in: 'query',
+                  name: 'from',
+                },
+                {
+                  in: 'query',
+                  name: 'to',
+                },
+                {
+                  in: 'query',
+                  name: 'amount',
+                },
+              ],
+            },
+          },
+        },
+        components: {
+          securitySchemes: {
+            'Currency Converter Security Scheme': {
+              in: 'query',
+              type: 'apiKey',
+              name: 'access_key',
+            },
+          },
+        },
+        security: {
+          'Currency Converter Security Scheme': [],
+        },
+      },
+      endpoints: [
+        {
+          name: 'convertToUSD',
+          operation: {
+            method: 'get',
+            path: '/convert',
+          },
+          fixedOperationParameters: [
+            {
+              operationParameter: {
+                in: 'query',
+                name: 'to',
+              },
+              value: 'USD',
+            },
+          ],
+          reservedParameters: [
+            {
+              name: '_type',
+              fixed: 'int256',
+            },
+            {
+              name: '_path',
+              fixed: 'result',
+            },
+            {
+              name: '_times',
+              default: '1000000',
+            },
+          ],
+          parameters: [
+            {
+              name: 'from',
+              default: 'EUR',
+              operationParameter: {
+                in: 'query',
+                name: 'from',
+              },
+            },
+            {
+              name: 'amount',
+              default: '1',
+              operationParameter: {
+                name: 'amount',
+                in: 'query',
+              },
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  apiCredentials: [
+    {
+      oisTitle: 'Currency Converter API',
+      securitySchemeName: 'Currency Converter Security Scheme',
+      securitySchemeValue: '${SS_CURRENCY_CONVERTER_API_KEY}',
+    },
+  ],
 });
 
 export const buildLocalSecrets = () => ({
@@ -112,9 +239,10 @@ export const buildLocalSecrets = () => ({
   CP_LOCAL_URL: 'http://127.0.0.1:8545/',
   HTTP_GATEWAY_API_KEY: 'some-api-key',
   HTTP_SIGNED_DATA_GATEWAY_URL: 'http://localhost:5432/signed-data-gateway/',
+  SS_CURRENCY_CONVERTER_API_KEY: 'some-api-key'
 });
 
-// Config for ETH subscription
+// Config for ETH subscription (Signed gateway)
 export const buildLocalConfigETH = () => ({
   airnodeMnemonic: 'achieve climb couple wait accident symbol spy blouse reduce foil echo label',
   privateKeys: {
@@ -137,7 +265,7 @@ export const buildLocalConfigETH = () => ({
   threshold: 10,
 });
 
-// Config for BTC subscription
+// Config for BTC subscription (Signed gateway)
 export const buildLocalConfigBTC = () => ({
   airnodeMnemonic: 'achieve climb couple wait accident symbol spy blouse reduce foil echo label',
   privateKeys: {
@@ -156,6 +284,29 @@ export const buildLocalConfigBTC = () => ({
     { type: 'string32', name: '_path', value: 'result' },
     { type: 'string32', name: '_times', value: '1000000' },
     { type: 'string32', name: 'from', value: 'BTC' },
+  ],
+  threshold: 10,
+});
+
+// Config for LTC subscription (Direct call)
+export const buildLocalConfigLTC = () => ({
+  airnodeMnemonic: 'achieve climb couple wait accident symbol spy blouse reduce foil echo label',
+  privateKeys: {
+    deployer: '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
+    manager: '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d',
+    sponsor: '0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a',
+    randomPerson: '0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a',
+  },
+  endpoint: {
+    oisTitle: 'Currency Converter API',
+    endpointName: 'convertToUSD',
+  },
+  templateParameters: [
+    { type: 'string32', name: 'to', value: 'USD' },
+    { type: 'string32', name: '_type', value: 'int256' },
+    { type: 'string32', name: '_path', value: 'result' },
+    { type: 'string32', name: '_times', value: '1000000' },
+    { type: 'string32', name: 'from', value: 'LTC' },
   ],
   threshold: 10,
 });
