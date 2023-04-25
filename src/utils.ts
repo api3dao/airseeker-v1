891f230e34369c52b8f2ab9c2146e4c618efb8d9
aquarat <blue@aquarat.co.za>
2023-04-25 16:06:54 +0100

    Fix unit tests

--- src/utils.ts
+++ src/utils.ts
@@ -10,6 +10,5 @@ export const calculateTimeout = (startTime: number, totalTimeout: number) => tot
 // We retry all chain operations with a random back-off infinitely until the next updates cycle
 export const prepareGoOptions = (_startTime: number, _totalTimeout: number): GoAsyncOptions => ({
   retries: INFINITE_RETRIES,
-  totalTimeoutMs: 1_000_000_000,
   delay: { type: 'random' as const, minDelayMs: RANDOM_BACKOFF_MIN_MS, maxDelayMs: RANDOM_BACKOFF_MAX_MS },
 });
