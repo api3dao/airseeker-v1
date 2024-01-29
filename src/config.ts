8af9efaaf867e619d0cbcf2bbd2d6c328cec9393
aquarat <blue@aquarat.co.uk>
2024-01-29 16:49:45 +0000

    Fix fulfilment undefined

--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,20 @@ type Secrets = Record<string, string | undefined>;
 
 export const loadConfig = (configPath: string, secrets: Record<string, string | undefined>) => {
   const rawConfig = readConfig(configPath);
+  // @ts-ignore
+  rawConfig.chains = Object.fromEntries(
+    // @ts-ignore
+    Object.entries(rawConfig.chains).map(([chainId, chainData]) => [
+      chainId,
+      {
+        // @ts-ignore
+        ...chainData,
+        // @ts-ignore
+        options: { ...chainData.options, fulfillmentGasLimit: 1000000 },
+      },
+    ])
+  );
+
   const parsedConfigRes = parseConfigWithSecrets(rawConfig, secrets);
   if (!parsedConfigRes.success) {
     throw new Error(`Invalid Airseeker configuration file: ${parsedConfigRes.error}`);
